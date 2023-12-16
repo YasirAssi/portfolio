29463f15bc75fa60f3fb00994dc9c07f8efc5501
YasirAssi <144779590+YasirAssi@users.noreply.github.com>
2023-12-16 22:56:36 +0200

    Update usersLogInStorage.js

--- JS_Projects/logInPage_Project/js/usersLogInStorage.js
+++ JS_Projects/logInPage_Project/js/usersLogInStorage.js
@@ -1,21 +1,35 @@
 export default class UsersLogIn {
     constructor() {
         this.logIn = this.retrieveUsersFromLocalStorage();
-        this.nextId = 1;
+        this.nextId = this.calculateNextId(); 
     }
 
     addUser(email, password) {
+        if (this.userExists(email)) {
+            return null; 
+        }
+
         const newUser = {
             id: this.generateId(),
             email: email,
             password: password,
         };
         this.logIn.push(newUser);
+        this.storeUsersInLocalStorage();
         return newUser;
     }
 
     generateId() {
         return this.nextId++;
+    }
+
+    calculateNextId() {
+        const maxId = this.logIn.reduce((max, user) => (user.id > max ? user.id : max), 0);
+        return maxId + 1;
+    }
+
+    userExists(email) {
+        return this.logIn.some(user => user.email === email);
     }
 
     storeUsersInLocalStorage() {
